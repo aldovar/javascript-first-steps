76baf1516beeaa8fe5aa42678b36a343a75c23a2
aldo.vargas <aldo.vargas@adesis.com>
2016-06-15 21:09:11 -0500

    Fibonacci fix

--- fibonacci.js
+++ fibonacci.js
@@ -4,23 +4,34 @@ function fibonacci(number){
     var fibonacci=0;
     
     for(var i=0; number>i;i++){
+        
         fibonacci=numberOne+numberTwo;
+        
         console.log(fibonacci);
+        
+        //El segundo numero de la suma siempre es igual primer numero antes de que cambie el valor de numero 1.
         numberTwo=numberOne;
+        
+        //El numero uno siempre es igual al resultado antes de que el valor cambie.
         numberOne=fibonacci;
     }
 }
 
 /**
-    1 + 0 = 1 
+  num1  num2  Resultado
+    1 +  0 =  1 
+      \
+        \
+    1 +  1 =  2 
+            /
+          /
+        /
+      /
+    2 +  1 =  3 
 
-    1 + 1 = 2 
+    3 +  2 =  5 
 
-    2 + 1 = 3 
+    5 +  3 =  8 
 
-    3 + 2 = 5 
-
-    5 + 3 = 8 
-
-    8 + 5 = 13 ... 
+    8 +  5 =  13 ... 
 **/
